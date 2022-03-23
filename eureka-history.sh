7447adff3f2389c0422ffadf27bbf037433b1702
Megan Mansfield <meganmansfield@arizona.edu>
2022-03-23 10:40:45 -0700

    removed steps to automatically run Eureka when conda environment is created

--- eureka-history.sh
+++ eureka-history.sh
@@ -11,19 +11,3 @@ git checkout 57cb0f1a576dfbbef9ba616b7a13a28c42efb876
 pip install .
 
 cd ../
-
-# grabbing the template run files
-# cp Eureka/demos/JWST/S2_nircam_wfss_template.ecf .
-cp Eureka/demos/JWST/S3_nircam_wfss_template.ecf .
-# cp Eureka/demos/JWST/S4_template.ecf .
-# cp Eureka/demos/JWST/S5_template.ecf .
-# cp Eureka/demos/JWST/S6_template.ecf .
-
-# changing hard coded things
-sed -i '' -e 's:/home/User/:'`pwd`':' S3_nircam_wfss_template.ecf
-
-sed -i '' -e 's:/Data/JWST-Sim/NIRCam/Stage2/:'/tiny/Stage2/':' S3_nircam_wfss_template.ecf
-sed -i '' -e 's:/Data/JWST-Sim/NIRCam/Stage3/:'/tiny/Stage3/':' S3_nircam_wfss_template.ecf
-
-# run stage 2 to stage 3
-python run_demo_eureka.py
